72beaa9d8206f078d0a4b90bf10b9da502578d2d
Robert M. Nill <rmnill202mail@gmail.com>
2024-09-29 17:13:02 -0400

    GET patients, with pagination

--- patient-dash-backend/src/index.ts
+++ patient-dash-backend/src/index.ts
@@ -1,6 +1,9 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { swagger } from '@elysiajs/swagger'
 import { authApiDetail, patientApiDetail, providerApiDetail, swaggerDocumentation } from "./constants/swagger";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient() 
 
 // URL Structure: //
 ////////////////////
@@ -15,14 +18,8 @@ import { authApiDetail, patientApiDetail, providerApiDetail, swaggerDocumentatio
   // PATCH
 // api/providers/:id
   // GET
-  
 
-  // .group("/auth", app => {
-  //       return app
-  //           .get("/", () => "Hi")
-  //           .post("/sign-in", ({ body }) => body)
-  //           .put("/sign-up", ({ body }) => body)
-  //   })
+
 
 const app = new Elysia()
   .use(swagger({ documentation: swaggerDocumentation }))
@@ -33,8 +30,65 @@ const app = new Elysia()
     .post("/register", () => "Register new provider account", authApiDetail)
   })
   .group("/api", app => app
-    .get("/patients", () => "GET Patients", patientApiDetail)
-    .post("/patients", () => "POSTing a new patient record", patientApiDetail)
+    .get("/patients", async ({query: {
+      count,
+      page
+    }}) => {
+      /**
+       * Parameters:
+       *    count - How many results are returned
+       *    page? - Which page we're on
+       * 
+       * Returns:
+       *    currentPage - The current page
+       *    finalPage - The final page with results
+       *    results - An array of patient data
+       */
+
+      // Prevent unnecessary query if page requested goes beyond total page count
+      const finalPage = Math.ceil(await prisma.patient.count() / count);
+      if(page && finalPage > page) {
+        return {
+          currentPage: finalPage,
+          finalPage,
+          results: []
+        };
+      }
+
+      const res = await prisma.patient.findMany({
+        take: count,
+        skip: count * ((page || 1) - 1),
+      });
+
+      return {
+        currentPage: page || 1,
+        finalPage,
+        results: res,
+      }
+    }, {
+      ...patientApiDetail,
+      query: t.Object({
+        count: t.Number(),
+        page: t.Optional(t.Number()),
+      })
+    })
+    .post("/patients", () => {
+      const res = prisma.patient.create({
+        data: {
+          // id: number;
+          // createdAt: Date;
+          // lastUpdated: Date;
+          // firstName: string;
+          // middleName: string | null;
+          // lastName: string;
+          // dateOfBirth: Date;
+          firstName: 'First Patient Name',
+          lastName: 'Last patient name',
+          dateOfBirth: new Date('01/01/2000'),
+        }
+      });
+      return res;
+    }, patientApiDetail)
     .get("/patients/:id", ({params: id}) => `GET Patient by ID ${id}`, patientApiDetail)
     .patch("/patients/:id", ({params: id}) => `PATCHing patient ${id}`, patientApiDetail)
     .get("/providers/:id", ({params: id}) => `GET provider by ID ${id}`, providerApiDetail)
